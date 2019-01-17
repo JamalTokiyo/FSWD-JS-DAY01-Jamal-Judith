3491ea25247699c0c82657e07f773edcf22b111b
JamalAhmed2019 <jamalusb@gmx.at>
2019-01-17 10:57:34 +0100

    Adding methods in java scrit

--- JAVA01.js
+++ JAVA01.js
@@ -8,3 +8,16 @@ var age= 25;
 console.log("my name is jamal " + age + "years old.");
 var players=["Martin","Thomas","Peter","Mathias", "Nick"];
 console.log("The most valuable player is "+ players[3]);
+
+// creating variable with method sort():
+ var carBrand=["Tesla","Audi","Renault","Volvo", "Mazda","Fiat", "Ferrari"];
+console.log(carBrand.sort());
+  // Variable with methods Pops and push, unshift shift 
+  var fruits=["Apple","Banane","Kiwi"];
+  fruits.push("Orange");
+  console.log(fruits);
+
+var fruits=["Apple","Banane","Kiwi" ,"Orange"];
+
+  console.log(fruits.pop());  
+  console.log(fruits);
