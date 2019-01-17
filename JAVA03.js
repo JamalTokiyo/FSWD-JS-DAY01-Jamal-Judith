790a656f66cca7661deefb79171e7eb0a7903828
Judith <judithmarialang@gmail.com>
2019-01-17 14:20:44 +0100

    adds exercise advanced 3

---
+++ JAVA03.js
@@ -0,0 +1,15 @@
+var box=[
+[1,2,1,24],
+[8,11,9,4],
+[7,0,7,27],
+[7,4,28,14],
+[3,10,25,7],
+[21,4,6,17],
+[3,5,26,3]
+];
+
+document.write(box[1][1] + "<br>")
+document.write(box[4][2] + "<br>")
+document.write(box[5][3] + "<br>")
+document.write(box[2][3] + "<br>")
+document.write(box[2][1] + "<br>")
