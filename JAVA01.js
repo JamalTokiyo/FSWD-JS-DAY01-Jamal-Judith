36d61d5b4276b1d6516ca701383c8b0e3831191b
Judith <judithmarialang@gmail.com>
2019-01-17 12:44:31 +0100

    adds advanced exercises

--- JAVA01.js
+++ JAVA01.js
@@ -40,7 +40,9 @@
 var b = "mango/cherries/kiwi/rapes/pear/peach/orange/lemon";
 var temp = new Array();
 temp = b.split('/');
-document.write(temp);
+document.write(temp[0] + "<br>" + temp[1] + "<br>" + temp[2] + "<br>" + temp [3]+ "<br>" + temp[4]+ "<br>" + temp[5]+ "<br>" + temp[6]+ "<br>" + temp[7]);
+
+temp[0];
 
 
 
