3d1c82c19e6828cac7fb01b78ae16801632a6443
Judith <judithmarialang@gmail.com>
2019-01-17 13:20:57 +0100

    adds Script

--- JAVA01.js
+++ JAVA01.js
@@ -38,7 +38,8 @@
 // console.log(basket);
 
 var b = "mango/cherries/kiwi/rapes/pear/peach/orange/lemon";
-var temp = new Array();
+// var temp = new Array();
+// no need to define a new array
 temp = b.split('/');
 document.write(temp[0] + "<br>" + temp[1] + "<br>" + temp[2] + "<br>" + temp [3]+ "<br>" + temp[4]+ "<br>" + temp[5]+ "<br>" + temp[6]+ "<br>" + temp[7]);
 
--- Javaa_advanced.js
+++ Javaa_advanced.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var a= 7;
+var b= 14;
+var c= 21;
+var d= 36;
+var e= 42;
+
+var sum=a+b+c+d+e;
+
+console.log(sum);
+
+document.write(sum);
+document.write ("The sum is");
