b050132c9fcb3df36b72a0cc232b35e5d659516a
Judith <judithmarialang@gmail.com>
2019-01-17 12:17:55 +0100

    adds file for exercise intermediate

--- JAVA01.js
+++ JAVA01.js
@@ -1,36 +1,36 @@
-console.log("my name is jamal ");
-document.write("May name is Judith");
-document.write("<br>");
-// alert("Hi Jamal!!")
+// console.log("my name is jamal ");
+// document.write("May name is Judith");
+// document.write("<br>");
+// // alert("Hi Jamal!!")
 
-var age= 25;
+// var age= 25;
 
-console.log("my name is jamal " + age + "years old.");
-var players=["Martin","Thomas","Peter","Mathias", "Nick"];
-console.log("The most valuable player is "+ players[3]);
+// console.log("my name is jamal " + age + "years old.");
+// var players=["Martin","Thomas","Peter","Mathias", "Nick"];
+// console.log("The most valuable player is "+ players[3]);
 
-// creating variable with method sort():
- var carBrand=["Tesla","Audi","Renault","Volvo", "Mazda","Fiat", "Ferrari"];
-console.log(carBrand.sort());
-  // Variable with methods Pops and push, unshift shift 
-  var fruits=["Apple","Banane","Kiwi"];
-  fruits.push("Orange");
-  console.log(fruits);
+// // creating variable with method sort():
+//  var carBrand=["Tesla","Audi","Renault","Volvo", "Mazda","Fiat", "Ferrari"];
+// console.log(carBrand.sort());
+//   // Variable with methods Pops and push, unshift shift 
+//   var fruits=["Apple","Banane","Kiwi"];
+//   fruits.push("Orange");
+//   console.log(fruits);
 
-var fruits=["Apple","Banane","Kiwi" ,"Orange"];
+// var fruits=["Apple","Banane","Kiwi" ,"Orange"];
 
-  console.log(fruits.pop());  
-  console.log(fruits);
+//   console.log(fruits.pop());  
+//   console.log(fruits);
 
-// sort and add animal - intermediate exercise 2
+// // sort and add animal - intermediate exercise 2
 
-var animals=["monkey","horse","dog"];
+// var animals=["monkey","horse","dog"];
 
-console.log(animals.sort());
+// console.log(animals.sort());
 
-animals.push("cat");
-console.log(animals);
-console.log(animals.sort());
+// animals.push("cat");
+// console.log(animals);
+// console.log(animals.sort());
 
 // intermediate exercise 3
 
