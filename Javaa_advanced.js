8bc3d79c9603ce6fd6a210096ff08c43006b2e76
JamalAhmed2019 <jamalusb@gmx.at>
2019-01-17 14:00:53 +0100

    addition Multi and division

--- Javaa_advanced.js
+++ Javaa_advanced.js
@@ -1,12 +1,27 @@
 var a= 7;
 var b= 14;
-var c= 21;
-var d= 36;
+var c= "21";
+var d= '36';
 var e= 42;
 
-var sum=a+b+c+d+e;
+Number(c);
 
-console.log(sum);
+var result=a+b+ Number(c)+ Number(d)+e;
 
-document.write(sum);
-document.write ("The sum is");
+
+document.write("The sum is" + result);
+document.write("<br>")
+// Multiplication with string number
+
+var f='1';
+var g=15;
+var h=8;
+var i="1";
+
+var multiResult= Number(f)*g*h*Number(i);
+document.write("The multiplication those numer and stings are:" + ' ' + multiResult)
+document.write("<br>")
+
+var div=result+multiResult;
+
+document.write("The division:" + div/2)
