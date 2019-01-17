eb7da15d4c3204a81720fa77652e175bc68d953d
Judith <judithmarialang@gmail.com>
2019-01-17 10:33:41 +0100

    adds Basic exercises

--- JAVA01.js
+++ JAVA01.js
@@ -1,3 +1,10 @@
 console.log("my name is jamal ");
 document.write("May name is Judith");
 document.write("<br>");
+// alert("Hi Jamal!!")
+
+var age= 25;
+
+console.log("my name is jamal " + age + "years old.");
+var players=["Martin","Thomas","Peter","Mathias", "Nick"];
+console.log("The most valuable player is "+ players[3]);
