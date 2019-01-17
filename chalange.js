e8e69d340dfab50ea66461b0ca134179ff949941
Judith <judithmarialang@gmail.com>
2019-01-17 15:17:41 +0100

    array changes

--- chalange.js
+++ chalange.js
@@ -1,5 +1,5 @@
 var wien=[
-["vienna","is a"],
-["nice", "City"],
+["vienna","is a","beautiful"],
+["nice", "City","in Europe"],
 ];
-document.write(wien[0][0] + ' ' + wien[0][1] + ' ' + wien[1][0] + ' ' + wien[1][1])
+document.write(wien[0][0] + ' ' + wien[0][1] + ' '+ wien[0][2] + ' ' + wien[1][0] + ' ' + wien[1][1] + ' ' + wien [1][2] + '.')
